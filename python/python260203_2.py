7b3428fac80ea19a49fb8583b44ec69be8b7b050
najongjine <najongjin3@hotmail.com>
2026-02-03 17:21:05 +0900

    .

--- python/python260203_2.py
+++ python/python260203_2.py
@@ -5,8 +5,10 @@ def func2():
     return 10
 
 def func3(a,b):
-    return a+b
+    return ++a + ++b
 
-a=[1,2]
-b=[2,3]
-print(func3(a,b))
+a=1
+b=2
+print(f"func3(a,b):{func3(a,b)}")
+print(f"a:{a}")
+print(f"b:{b}")
