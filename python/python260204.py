d1f813d789e918bd2adc540bfb1d71f61f2afe36
najongjine <najongjin3@hotmail.com>
2026-02-04 16:34:08 +0900

    .

--- python/python260204.py
+++ python/python260204.py
@@ -1,8 +1,11 @@
 class Animal:
-    # 파이썬에선 생성자 함수 이름을 통일했어요.
-    # 자바에 있는 생성자 오버로딩 없어요
+    """ 
+    파이썬에선 생성자 함수 이름을 __init__ 으로 통일했어요.
+    __init__(self) 생성자 에서 self 키워드는 무조건 넣어 줘야해요
+    자바에 있는 생성자 오버로딩 없어요
+    """
     def __init__(self,leg=4,fur=True):
-
+        self.leg=leg
         pass
     
 
